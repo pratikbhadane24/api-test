5f1eb6f5cbcb215ad277ab26ed871f90e91fd43e
Pratik Bhadane <pratikbhadane24@gmail.com>
2025-10-07 15:17:10 +0530

    Implement periodic IP health checks in a separate thread

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from typing import Union
+import time
+import threading
 import requests
-
 from fastapi import FastAPI
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
@@ -16,3 +15,16 @@ def ip_tester():
     req = requests.get("https://signals.cirrus.trade/health")
     print("REQUEST", req)
     return {"status": req.status_code, "response": req.json()}
+
+
+def repeat_ip_test():
+    while True:
+        try:
+            response = ip_tester()
+            print("Periodic Check:", response)
+        except Exception as e:
+            print("Error during IP test:", e)
+        time.sleep(30)
+
+# Start the loop in a separate thread
+threading.Thread(target=repeat_ip_test, daemon=True).start()
